1f9b7caba856b8110dc88477991c7a03b899675e
jeongeunkill <way_1125@naver.com>
2025-09-29 15:13:51 +0900

    fix: set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PORTFOLIO/',   // 🔥 리포 이름과 정확히 일치 (대소문자 주의)
 })
