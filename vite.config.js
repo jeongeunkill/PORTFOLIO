63e72f797e887725c2dc0814b88fb0c07130cae0
jeongeunkill <way_1125@naver.com>
2025-09-30 11:08:16 +0900

    update: fix 6

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/PORTFOLIO/',   // ← 레포 이름
   plugins: [react()],
-  base: '/PORTFOLIO/',   // 🔥 리포 이름과 정확히 일치 (대소문자 주의)
 })
